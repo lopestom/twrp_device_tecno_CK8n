86584b6b09ca4eb821d1523ff8982d33d8e0f22e
lopestorm <guillopes2002@gmail.com>
2023-08-09 22:26:27 -0300

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -19,6 +19,9 @@ DEVICE_PATH := device/tecno/CK8n
 # For building with minimal manifest
 ALLOW_MISSING_DEPENDENCIES := true
 
+# Build Hack
+BUILD_BROKEN_DUP_RULES := true
+
 # Architecture
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
